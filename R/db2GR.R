8c9038f3cdbd86eff89d3dd043f99dd452aa8ad9
CreRecombinase <nwknoblauch@gmail.com>
2019-04-16 16:29:43 -0400

    tweaked syntax

--- R/db2GR.R
+++ R/db2GR.R
@@ -10,50 +10,75 @@ outf <- snakemake@output[["outf"]]
 beta_v <- snakemake@params[["beta_v"]]
 se_v <- snakemake@params[["se_v"]]
 
-ld_df <- read_tsv(input_f,col_types=cols(
+ld_df <- read_tsv(input_f, col_types = cols(
                               chr = col_character(),
                               start = col_integer(),
                               stop = col_integer()
                           )) %>%
-    rename(chrom=chr) %>%
+    rename(chrom = chr) %>%
     group_by(chrom) %>%
-    mutate(start = if_else(start==min(start),
-                           0L,start)) %>%
-    mutate(stop = if_else(stop==max(stop),
-                          .Machine$integer.max,stop)) %>%
+    mutate(start = if_else(start == min(start),
+                           0L, start)) %>%
+    mutate(stop = if_else(stop == max(stop),
+                          .Machine$integer.max, stop)) %>%
     ungroup() %>%
-    mutate(region_id=1:n())
+    mutate(region_id = 1:n())
 
 
 stopifnot(file.exists(input_db))
 output_txtf <- snakemake@output[["txtf"]]
 output_rds <- snakemake@output[["rds"]]
-gwas_df <- dplyr::tbl(dplyr::src_sqlite(path=input_db,create=F),"gwas") %>% collect()
-gwas_z <- dplyr::select(gwas_df,SNP=id,chrom,pos,beta=!!beta_v,se=!!se_v) %>%
-    dplyr::mutate(`z-val`=beta/se)
+gwas_df <- dplyr::tbl(dplyr::src_sqlite(path = input_db, create = F),
+                      "gwas") %>%
+    collect()
+gwas_z <- dplyr::select(gwas_df,
+                        SNP = id,
+                        chrom, pos,
+                        beta = !!beta_v,
+                        se = !!se_v) %>%
+    dplyr::mutate(`z-val` = beta / se)
 
 
-m_gwas_df <- select(gwas_df,chrom,pos,MAJOR=A1,MINOR=A2) %>%
-    mutate(chrom=as.integer(chrom),pos=as.integer(pos),SNP=paste0(chrom,":",pos))
+m_gwas_df <- select(gwas_df,
+                    chrom,
+                    pos,
+                    MAJOR = A1,
+                    MINOR = A2) %>%
+    mutate(chrom = as.integer(chrom),
+           pos = as.integer(pos),
+           SNP = paste0(chrom, ":", pos))
 
 snp_df <- gwas_z %>%
-    select(SNP,chr = chrom, pos, `z-val`) %>%
-    mutate(chr = as.integer(gsub("chr","",chr)),pos=as.integer(pos))  %>%
-    distinct(chr,pos,.keep_all=T) %>%
-    arrange(chr,pos) %>%
-    ldshrink::assign_snp_block(break_df=mutate(ld_df,chr = as.integer(gsub("chr","",chrom))), assign_all=T)
+    select(SNP, chr = chrom, pos, `z-val`) %>%
+    mutate(chr = as.integer(gsub("chr", "", chr)), pos = as.integer(pos))  %>%
+    distinct(chr, pos, .keep_all = T) %>%
+    arrange(chr, pos)
+snp_df <- mutate(snp_df,
+                 region_id = assign_snp_block(break_chr = ld_df$ch,
+                                              break_start = ld_df$start,
+                                              break_stop = ld_df$stop,
+                                              break_id = ld_df$region_id,
+                                              snp_chr = chr,
+                                              pos = pos,
+                                              assign_all = T))
+
 snp_df <- dplyr::group_by(snp_df, chr, region_id) %>%
-    dplyr::summarise(ct=n()) %>%
+    dplyr::summarise(ct = n()) %>%
     dplyr::ungroup() %>%
     dplyr::inner_join(snp_df) %>%
-    dplyr::mutate(region_id = ifelse(ct<min_size, region_id-1, region_id)) %>%
+    dplyr::mutate(region_id = ifelse(ct < min_size,
+                                     region_id - 1,
+                                     region_id)) %>%
     dplyr::select(-ct) %>%
     dplyr::ungroup()
 
-snp_df%>%
-    select(SNP,locus=region_id,`z-val`) %>%
+snp_df %>%
+    select(SNP, locus = region_id, `z-val`) %>%
     write_tsv(output_txtf)
-GR_df <- select(gwas_z,chrom,start=pos) %>%
-    mutate(chrom=paste0("chr",chrom),start=as.integer(start),end=start+1L) %>%
+gr_df <- select(gwas_z, chrom, start = pos) %>%
+    mutate(chrom = paste0("chr", chrom),
+           start = as.integer(start),
+           end = start + 1L) %>%
     GenomicRanges::makeGRangesFromDataFrame()
-saveRDS(GR_df,output_rds)
+
+saveRDS(gr_df, output_rds)
